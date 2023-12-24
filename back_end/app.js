f56a2a3b075d1c3ddd644f19d3bfe158dc9bb477
NAPHAT CHAIYASANG <6431503016@lamduan.mfu.ac.th>
2023-12-24 22:24:09 +0700

    set xampp

---
+++ back_end/app.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql');
+
+// กำหนดค่าการเชื่อมต่อ MySQL
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'user_management_db',
+});
+
+// เชื่อมต่อกับ MySQL
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('Connected to MySQL');
+});
+
+// ทำ query ตามต้องการ
+connection.query('SELECT * FROM users', (error, results, fields) => {
+  if (error) throw error;
+  console.log('Query results:', results);
+});
+
+// ปิดการเชื่อมต่อ MySQL เมื่อทำงานเสร็จ
+connection.end();
