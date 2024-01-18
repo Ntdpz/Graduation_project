c405ec41c129a93b9c31d3e192916f5884fcab51
NZ_NTDPZZ <6431503016@lamduan.mfu.ac.th>
2024-01-17 22:54:22 -0800

    set Database

--- back_end/app.js
+++ back_end/app.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'user_management_db',
+  database: 'graduation_project',
 });
 
 // เชื่อมต่อกับ MySQL
