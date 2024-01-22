383a9fbd499f116e10b8ca31035e43417dd65f0d
NZ_NTDPZZ <6431503016@lamduan.mfu.ac.th>
2024-01-22 23:03:02 +0700

    set dataabse Conect Cors

--- back_end/app.js
+++ back_end/app.js
@@ -1,14 +1,18 @@
 // Import express module
 const express = require('express');
+const cors = require('cors');
 
-// Import userRoutes module (ที่เราสร้างไว้)
+// Import userRoutes module
 const userRoutes = require('./routes/userRoutes');
 
-// Import db module (ที่เราสร้างไว้)
-const db = require('./module/db');
+// Import db module
+const { connectToDatabase } = require('./modules/db');
 
 // Create an Express application
 const app = express();
+
+// Set up CORS middleware
+app.use(cors());
 
 // Set the port to 8080
 const port = 8080;
@@ -16,7 +20,11 @@ const port = 8080;
 // Use routes (สร้าง URL prefix '/api' สำหรับ userRoutes)
 app.use('/api', userRoutes);
 
-// Start the server and listen on port 8080
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// Connect to the database and start the server
+connectToDatabase().then(() => {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}).catch(error => {
+  console.error('Error connecting to the database:', error);
 });
