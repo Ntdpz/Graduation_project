5169fd6d207e79c5d543e0a983b1c38b8e392b1b
Naphat <6431503016@lamduan.mfu.ac.th>
2024-02-03 14:21:01 +0700

    WEEK 1

--- back_end/routes/screensRoutes.js
+++ back_end/routes/screensRoutes.js
@@ -1,138 +1,169 @@
 const express = require('express');
 const router = express.Router();
-const { db } = require('../modules/db');
+const path = require('path');
+const { db, connectToDatabase } = require(path.join(__dirname, '../modules/db'));
 
-// Get all screens
-router.get('/screens', async (req, res) => {
-    try {
-        const query = 'SELECT * FROM Screens';
-        const screens = await new Promise((resolve, reject) => {
-            db.query(query, (err, results) => {
-                if (err) reject(err);
-                resolve(results);
-            });
-        });
-        res.json(screens);
-    } catch (error) {
-        console.error('Error fetching screens:', error);
-        res.status(500).send('Internal Server Error');
-    }
+router.use(express.json());
+
+router.use(async (req, res, next) => {
+  try {
+    await connectToDatabase();
+    next();
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
-// Route สำหรับดึงข้อมูล Screen ด้วย ID
-router.get('/screens/:screen_id', async (req, res) => {
-    try {
-        const { screen_id } = req.params;
 
-        const query = 'SELECT * FROM Screens WHERE screen_id = ?';
+router.post('/projects', async (req, res) => {
+  try {
+    const {
+      project_id,
+      project_name_TH,
+      project_name_ENG,
+      project_progress,
+      project_plan_start,
+      project_plan_end,
+    } = req.body;
 
-        const screen = await new Promise((resolve, reject) => {
-            db.query(query, [screen_id], (err, results) => {
-                if (err) reject(err);
-                resolve(results);
-            });
-        });
+    const query =
+      'INSERT INTO Projects (project_id, project_name_TH, project_name_ENG, project_progress, project_plan_start, project_plan_end) VALUES (?, ?, ?, ?, ?, ?)';
 
-        if (screen.length === 0) {
-            res.status(404).json({ error: 'Screen not found' });
-        } else {
-            res.json(screen[0]);
+    await new Promise((resolve, reject) => {
+      db.query(
+        query,
+        [project_id, project_name_TH, project_name_ENG, project_progress, project_plan_start, project_plan_end],
+        (err, result) => {
+          if (err) reject(err);
+          resolve(result);
         }
-    } catch (error) {
-        console.error('Error fetching screen by ID:', error);
-        res.status(500).send('Internal Server Error');
-    }
+      );
+    });
+
+    res.send('Project created successfully');
+  } catch (error) {
+    console.error('Error creating project:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-// Create a new screen
-router.post('/screens', async (req, res) => {
-    try {
-        const { screen_id, screen_name, screen_status, screen_level, screen_manday, system_id, project_id, screen_progress, screen_plan_start, screen_plan_end, screen_actual_start, screen_actual_end, screen_pic } = req.body;
+// Route สำหรับดึงข้อมูลโปรเจ็คทั้งหมด
+router.get('/projects', async (req, res) => {
+  try {
+    const query = 'SELECT * FROM Projects';
 
-        const query = 'INSERT INTO Screens (screen_id, screen_name, screen_status, screen_level, screen_manday, system_id, project_id, screen_progress, screen_plan_start, screen_plan_end, screen_actual_start, screen_actual_end, screen_pic) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    const results = await new Promise((resolve, reject) => {
+      db.query(query, (err, results) => {
+        if (err) reject(err);
+        resolve(results);
+      });
+    });
 
-        await new Promise((resolve, reject) => {
-            db.query(
-                query,
-                [screen_id, screen_name, screen_status, screen_level, screen_manday, system_id, project_id, screen_progress, screen_plan_start, screen_plan_end, screen_actual_start, screen_actual_end, screen_pic],
-                (err, result) => {
-                    if (err) reject(err);
-                    resolve(result);
-                }
-            );
-        });
-
-        res.send('Screen created successfully');
-    } catch (error) {
-        console.error('Error creating screen:', error);
-        res.status(500).send('Internal Server Error');
-    }
+    res.json(results);
+  } catch (error) {
+    console.error('Error fetching projects:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-// Update an existing screen
-router.put('/screens/:screen_id', async (req, res) => {
-    try {
-        const { screen_id } = req.params;
-        const { screen_name, screen_status, screen_level, screen_manday, system_id, project_id, screen_progress, screen_plan_start, screen_plan_end, screen_actual_start, screen_actual_end, screen_pic } = req.body;
+// Route สำหรับดึงข้อมูลโปรเจ็คด้วย ID
+router.get('/projects/:project_id', async (req, res) => {
+  try {
+    const { project_id } = req.params;
 
-        const updatedScreenFields = {};
+    const query = 'SELECT * FROM Projects WHERE project_id = ?';
 
-        if (screen_name !== undefined) updatedScreenFields.screen_name = screen_name;
-        if (screen_status !== undefined) updatedScreenFields.screen_status = screen_status;
-        if (screen_level !== undefined) updatedScreenFields.screen_level = screen_level;
-        if (screen_manday !== undefined) updatedScreenFields.screen_manday = screen_manday;
-        if (system_id !== undefined) updatedScreenFields.system_id = system_id;
-        if (project_id !== undefined) updatedScreenFields.project_id = project_id;
-        if (screen_progress !== undefined) updatedScreenFields.screen_progress = screen_progress;
-        if (screen_plan_start !== undefined) updatedScreenFields.screen_plan_start = screen_plan_start;
-        if (screen_plan_end !== undefined) updatedScreenFields.screen_plan_end = screen_plan_end;
-        if (screen_actual_start !== undefined) updatedScreenFields.screen_actual_start = screen_actual_start;
-        if (screen_actual_end !== undefined) updatedScreenFields.screen_actual_end = screen_actual_end;
-        if (screen_pic !== undefined) updatedScreenFields.screen_pic = screen_pic;
+    const results = await new Promise((resolve, reject) => {
+      db.query(query, [project_id], (err, results) => {
+        if (err) reject(err);
+        resolve(results);
+      });
+    });
 
-        const query = 'UPDATE Screens SET ? WHERE screen_id = ?';
-
-        await new Promise((resolve, reject) => {
-            db.query(
-                query,
-                [updatedScreenFields, screen_id],
-                (err, result) => {
-                    if (err) {
-                        console.error('Error updating screen:', err);
-                        reject(err);
-                    } else {
-                        resolve(result);
-                    }
-                }
-            );
-        });
-
-
-        res.send('Screen updated successfully');
-    } catch (error) {
-        console.error('Error updating screen:', error);
-        res.status(500).send('Internal Server Error');
+    if (results.length === 0) {
+      res.status(404).json({ error: 'Project not found' });
+    } else {
+      res.json(results[0]);
     }
+  } catch (error) {
+    console.error('Error fetching project by ID:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-// Delete an existing screen
-router.delete('/screens/:screen_id', async (req, res) => {
-    try {
-        const { screen_id } = req.params;
 
-        const query = 'DELETE FROM Screens WHERE screen_id = ?';
+router.put('/projects/:project_id', async (req, res) => {
+  try {
+    const {
+      project_name_TH,
+      project_name_ENG,
+      project_progress,
+      project_plan_start,
+      project_plan_end,
+    } = req.body;
 
-        await new Promise((resolve, reject) => {
-            db.query(query, [screen_id], (err, result) => {
-                if (err) reject(err);
-                resolve(result);
-            });
-        });
+    const { project_id } = req.params;
 
-        res.send('Screen deleted successfully');
-    } catch (error) {
-        console.error('Error deleting screen:', error);
-        res.status(500).send('Internal Server Error');
+    const updatedProjectFields = {};
+
+    if (project_name_TH !== undefined) {
+      updatedProjectFields.project_name_TH = project_name_TH;
     }
+
+    if (project_name_ENG !== undefined) {
+      updatedProjectFields.project_name_ENG = project_name_ENG;
+    }
+
+    if (project_progress !== undefined) {
+      updatedProjectFields.project_progress = project_progress;
+    }
+
+    if (project_plan_start !== undefined) {
+      updatedProjectFields.project_plan_start = project_plan_start;
+    }
+
+    if (project_plan_end !== undefined) {
+      updatedProjectFields.project_plan_end = project_plan_end;
+    }
+
+    if (Object.keys(updatedProjectFields).length === 0) {
+      return res.status(400).json({ error: 'No fields to update' });
+    }
+
+    const query = 'UPDATE Projects SET ? WHERE project_id = ?';
+
+    await new Promise((resolve, reject) => {
+      db.query(query, [updatedProjectFields, project_id], (err, result) => {
+        if (err) reject(err);
+        resolve(result);
+      });
+    });
+
+    res.send('Project updated successfully');
+  } catch (error) {
+    console.error('Error updating project:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+router.delete('/projects/:project_id', async (req, res) => {
+  try {
+    const { project_id } = req.params;
+
+    const query = 'DELETE FROM Projects WHERE project_id = ?';
+
+    await new Promise((resolve, reject) => {
+      db.query(query, [project_id], (err, result) => {
+        if (err) reject(err);
+        resolve(result);
+      });
+    });
+
+    res.send('Project deleted successfully');
+  } catch (error) {
+    console.error('Error deleting project:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 module.exports = router;
--- back_end/routes/userRoutes.js
+++ back_end/routes/userRoutes.js
@@ -18,18 +18,109 @@ router.use(async (req, res, next) => {
   }
 });
 
-//login API
-router.post('/login', async (req, res) => {
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-  const storage = multer.memoryStorage();
-  const upload = multer({ storage: storage });
+// Middleware สำหรับการอัปโหลดไฟล์ภาพ
+const uploadSingle = upload.single('user_pic');
 
-  // Middleware สำหรับการอัปโหลดไฟล์ภาพ
-  const uploadSingle = upload.single('user_pic');
+router.put('/users/:user_id', async (req, res) => {
+  console.log(req.body);
+  try {
+    const {
+      user_firstname,
+      user_lastname,
+      user_position,
+      user_department,
+      user_email,
+      user_password,
+      user_status,
+      user_role
+    } = req.body;
 
-  router.put('/users/:user_id', async (req, res) => {
-    console.log(req.body);
-    try {
+    const { user_id } = req.params;
+
+    // Create an object to store all fields that need to be updated
+    const updatedUserFields = {};
+
+    // Check and add user_firstname if provided
+    if (user_firstname !== undefined) {
+      updatedUserFields.user_firstname = user_firstname;
+    }
+
+    // Check and add user_lastname if provided
+    if (user_lastname !== undefined) {
+      updatedUserFields.user_lastname = user_lastname;
+    }
+
+    // Check and add user_position if provided
+    if (user_position !== undefined) {
+      updatedUserFields.user_position = user_position;
+    }
+
+    // Check and add user_department if provided
+    if (user_department !== undefined) {
+      updatedUserFields.user_department = user_department;
+    }
+
+    // Check and add user_email if provided
+    if (user_email !== undefined) {
+      updatedUserFields.user_email = user_email;
+    }
+
+    // Check and add user_password if provided
+    if (user_password !== undefined) {
+      updatedUserFields.user_password = user_password;
+    }
+
+    // Check and add user_status if provided
+    if (user_status !== undefined) {
+      updatedUserFields.user_status = user_status;
+    }
+
+    // Check and add user_role if provided
+    if (user_role !== undefined) {
+      updatedUserFields.user_role = user_role;
+    }
+
+    // Check if there are fields to update
+    if (Object.keys(updatedUserFields).length === 0) {
+      return res.status(400).json({ error: 'No fields to update' });
+    }
+
+    const query = 'UPDATE Users SET ? WHERE user_id = ?';
+
+    await new Promise((resolve, reject) => {
+      db.query(
+        query,
+        [updatedUserFields, user_id],
+        (err, result) => {
+          if (err) reject(err);
+          resolve(result);
+        }
+      );
+    });
+
+    res.send('User updated successfully');
+  } catch (error) {
+    console.error('Error updating user:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+// Route สำหรับสร้างผู้ใช้ใหม่
+router.post('/users', async (req, res) => {
+  try {
+    const { Users } = req.body;
+
+    // Check if Users array is provided
+    if (!Users || !Array.isArray(Users)) {
+      return res.status(400).json({ error: 'Users array is required' });
+    }
+
+    // Loop through each user in the array
+    for (const user of Users) {
       const {
         user_firstname,
         user_lastname,
@@ -38,222 +129,128 @@ router.post('/login', async (req, res) => {
         user_email,
         user_password,
         user_status,
-        user_role
-      } = req.body;
+        user_role,
+        user_pic,
+        user_id,
+      } = user;
 
-      const { user_id } = req.params;
-
-      // Create an object to store all fields that need to be updated
-      const updatedUserFields = {};
-
-      // Check and add user_firstname if provided
-      if (user_firstname !== undefined) {
-        updatedUserFields.user_firstname = user_firstname;
+      // Check if user_id is provided for each user
+      if (!user_id) {
+        return res.status(400).json({ error: 'user_id is required for each user' });
       }
 
-      // Check and add user_lastname if provided
-      if (user_lastname !== undefined) {
-        updatedUserFields.user_lastname = user_lastname;
-      }
+      const userPosition = user_position || 'Default Position';
 
-      // Check and add user_position if provided
-      if (user_position !== undefined) {
-        updatedUserFields.user_position = user_position;
-      }
-
-      // Check and add user_department if provided
-      if (user_department !== undefined) {
-        updatedUserFields.user_department = user_department;
-      }
-
-      // Check and add user_email if provided
-      if (user_email !== undefined) {
-        updatedUserFields.user_email = user_email;
-      }
-
-      // Check and add user_password if provided
-      if (user_password !== undefined) {
-        updatedUserFields.user_password = user_password;
-      }
-
-      // Check and add user_status if provided
-      if (user_status !== undefined) {
-        updatedUserFields.user_status = user_status;
-      }
-
-      // Check and add user_role if provided
-      if (user_role !== undefined) {
-        updatedUserFields.user_role = user_role;
-      }
-
-      // Check if there are fields to update
-      if (Object.keys(updatedUserFields).length === 0) {
-        return res.status(400).json({ error: 'No fields to update' });
-      }
-
-      const query = 'UPDATE Users SET ? WHERE user_id = ?';
+      const query =
+        'INSERT INTO Users (user_firstname, user_lastname, user_position, user_department, user_email, user_password, user_status, user_role, user_pic, user_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
 
       await new Promise((resolve, reject) => {
         db.query(
           query,
-          [updatedUserFields, user_id],
+          [
+            user_firstname,
+            user_lastname,
+            userPosition,
+            user_department,
+            user_email,
+            user_password,
+            user_status,
+            user_role,
+            user_pic,
+            user_id,
+          ],
           (err, result) => {
             if (err) reject(err);
             resolve(result);
           }
         );
       });
-
-      res.send('User updated successfully');
-    } catch (error) {
-      console.error('Error updating user:', error);
-      res.status(500).send('Internal Server Error');
     }
+
+    res.send('Users created successfully');
+  } catch (error) {
+    console.error('Error creating users:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+// Function to read a file and convert its content to base64
+async function getFileAsBase64(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, { encoding: 'base64' }, (err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    });
   });
+};
 
 
-  // Route สำหรับสร้างผู้ใช้ใหม่
-  router.post('/users', async (req, res) => {
-    try {
-      const { Users } = req.body;
 
-      // Check if Users array is provided
-      if (!Users || !Array.isArray(Users)) {
-        return res.status(400).json({ error: 'Users array is required' });
-      }
+// Route สำหรับลบผู้ใช้
+router.delete('/users/:user_id', async (req, res) => {
+  try {
+    const { user_id } = req.params;
 
-      // Loop through each user in the array
-      for (const user of Users) {
-        const {
-          user_firstname,
-          user_lastname,
-          user_position,
-          user_department,
-          user_email,
-          user_password,
-          user_status,
-          user_role,
-          user_pic,
-          user_id,
-        } = user;
+    const query = 'DELETE FROM Users WHERE user_id = ?';
 
-        // Check if user_id is provided for each user
-        if (!user_id) {
-          return res.status(400).json({ error: 'user_id is required for each user' });
-        }
-
-        const userPosition = user_position || 'Default Position';
-
-        const query =
-          'INSERT INTO Users (user_firstname, user_lastname, user_position, user_department, user_email, user_password, user_status, user_role, user_pic, user_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-
-        await new Promise((resolve, reject) => {
-          db.query(
-            query,
-            [
-              user_firstname,
-              user_lastname,
-              userPosition,
-              user_department,
-              user_email,
-              user_password,
-              user_status,
-              user_role,
-              user_pic,
-              user_id,
-            ],
-            (err, result) => {
-              if (err) reject(err);
-              resolve(result);
-            }
-          );
-        });
-      }
-
-      res.send('Users created successfully');
-    } catch (error) {
-      console.error('Error creating users:', error);
-      res.status(500).send('Internal Server Error');
-    }
-  });
-
-
-  // Function to read a file and convert its content to base64
-  async function getFileAsBase64(filePath) {
-    return new Promise((resolve, reject) => {
-      fs.readFile(filePath, { encoding: 'base64' }, (err, data) => {
+    await new Promise((resolve, reject) => {
+      db.query(query, [user_id], (err, result) => {
         if (err) reject(err);
-        resolve(data);
+        resolve(result);
       });
     });
-  };
 
-
-
-  // Route สำหรับลบผู้ใช้
-  router.delete('/users/:user_id', async (req, res) => {
-    try {
-      const { user_id } = req.params;
-
-      const query = 'DELETE FROM Users WHERE user_id = ?';
-
-      await new Promise((resolve, reject) => {
-        db.query(query, [user_id], (err, result) => {
-          if (err) reject(err);
-          resolve(result);
-        });
-      });
-
-      res.send('User deleted successfully');
-    } catch (error) {
-      console.error('Error deleting user:', error);
-      res.status(500).send('Internal Server Error');
-    }
-  });
-
-  // Route สำหรับดึงข้อมูลผู้ใช้ทั้งหมด
-  router.get('/users', async (req, res) => {
-    try {
-      await connectToDatabase();
-      const query = 'SELECT * FROM Users';
-
-      const results = await new Promise((resolve, reject) => {
-        db.query(query, (err, results) => {
-          if (err) reject(err);
-          resolve(results);
-        });
-      });
-
-      res.json(results);
-    } catch (error) {
-      console.error('Error fetching users:', error);
-      res.status(500).send('Internal Server Error');
-    }
-  });
-  // Route สำหรับดึงข้อมูลผู้ใช้ด้วย ID
-  router.get('/users/:user_id', async (req, res) => {
-    try {
-      await connectToDatabase();
-      const { user_id } = req.params;
-
-      const query = 'SELECT * FROM Users WHERE user_id = ?';
-
-      const results = await new Promise((resolve, reject) => {
-        db.query(query, [user_id], (err, results) => {
-          if (err) reject(err);
-          resolve(results);
-        });
-      });
-
-      if (results.length === 0) {
-        res.status(404).json({ error: 'User not found' });
-      } else {
-        res.json(results[0]);
-      }
-    } catch (error) {
-      console.error('Error fetching user by ID:', error);
-      res.status(500).send('Internal Server Error');
-    }
-  });
+    res.send('User deleted successfully');
+  } catch (error) {
+    console.error('Error deleting user:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
+
+// Route สำหรับดึงข้อมูลผู้ใช้ทั้งหมด
+router.get('/users', async (req, res) => {
+  try {
+    await connectToDatabase();
+    const query = 'SELECT * FROM Users';
+
+    const results = await new Promise((resolve, reject) => {
+      db.query(query, (err, results) => {
+        if (err) reject(err);
+        resolve(results);
+      });
+    });
+
+    res.json(results);
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+// Route สำหรับดึงข้อมูลผู้ใช้ด้วย ID
+router.get('/users/:user_id', async (req, res) => {
+  try {
+    await connectToDatabase();
+    const { user_id } = req.params;
+
+    const query = 'SELECT * FROM Users WHERE user_id = ?';
+
+    const results = await new Promise((resolve, reject) => {
+      db.query(query, [user_id], (err, results) => {
+        if (err) reject(err);
+        resolve(results);
+      });
+    });
+
+    if (results.length === 0) {
+      res.status(404).json({ error: 'User not found' });
+    } else {
+      res.json(results[0]);
+    }
+  } catch (error) {
+    console.error('Error fetching user by ID:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 module.exports = router;
