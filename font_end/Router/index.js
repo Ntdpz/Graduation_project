66970e8a5502109755a439cec3a598645115512e
WITSAKON LOTHANARUNGROT <6431503055@lamduan.mfu.ac.th>
2024-02-04 03:19:53 +0700

    edit index.js

--- font_end/Router/index.js
+++ font_end/Router/index.js
@@ -1,11 +1,32 @@
 // main.js
+// router/index.js
 import Vue from 'vue';
-import App from './App.vue';
-import router from '@/router/index';
+import VueRouter from 'vue-router';
+import EditUser from '@/pages/User/EditUser.vue';
+import HomePageAdmin from './components/HomePageAdmin.vue';
+import ManageUser from './components/ManageUser.vue';
+import ManageProject from './components/ManageProject.vue';
+import UserManage from './components/UserManage.vue';
+import CreateProject from './pages/Project/createProject.vue';
 
-Vue.config.productionTip = false;
 
-new Vue({
-  render: h => h(App),
-  router
-}).$mount('#app');
+Vue.use(VueRouter);
+
+export const routes = [
+  { path: '/', redirect: '/admin' },  // Redirect root to /admin
+  { path: '/admin', component: HomePageAdmin },
+  { path: '/manage_user', component: ManageUser },
+  { path: '/manage_project', component: ManageProject },
+  { path: '/UserManage', component: UserManage },
+  { path: '/User/editUser/:id', name: 'edit-user', component: EditUser },
+  { path: '/', component: Dashboard },
+  { path: '/project', omponent: {render(c) {
+                return c('router-view');
+            },},children: [{ path: 'create-project', component: CreateProject },],},
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
