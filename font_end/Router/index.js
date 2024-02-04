b0f5f44e5f66604cbf4cbed978a4471fced0807c
WITSAKON LOTHANARUNGROT <6431503055@lamduan.mfu.ac.th>
2024-02-05 06:26:32 +0700

    edit index.js

--- font_end/Router/index.js
+++ font_end/Router/index.js
@@ -8,8 +8,8 @@ import ManageUser from './components/ManageUser.vue';
 import ManageProject from './components/ManageProject.vue';
 import UserManage from './components/UserManage.vue';
 import CreateProject from './pages/Project/createProject.vue';
-
-
+import EditProject from '@/pages/Project/EditProject.vue';  // Import your EditProject component
+import ProjectManagement from '@/pages/Project/ProjectManagement.vue';  // Import your ProjectManagement component
 Vue.use(VueRouter);
 
 export const routes = [
@@ -19,9 +19,9 @@ export const routes = [
   { path: '/manage_project', component: ManageProject },
   { path: '/UserManage', component: UserManage },
   { path: '/User/editUser/:id', name: 'edit-user', component: EditUser },
-  { path: '/', component: Dashboard },
   { path: '/project/createProject', component: CreateProject },
-            
+  { path: '/project/editProject/:id', name: 'edit-project', component: EditProject },  // Add this route
+  { path: '/project-management', name: 'project-management', component: ProjectManagement },  // Add a name to the route         
 ];
 
 const router = new VueRouter({
