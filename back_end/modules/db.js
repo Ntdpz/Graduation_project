3a0331d54f77b49333bb68da253baec5862301dc
Naphat <6431503016@lamduan.mfu.ac.th>
2024-02-08 03:39:40 +0700

    Chang batabase

--- back_end/modules/db.js
+++ back_end/modules/db.js
@@ -1,21 +1,21 @@
 const mysql = require('mysql');
 
-// สร้าง pool connection ไปยังฐานข้อมูล MySQL
-// const db = mysql.createPool({
-//   connectionLimit: 10, // จำนวน connection สูงสุดที่ pool สามารถจัดการได้
-//   host: 'mysql-seniorprojectprogresstracking.alwaysdata.net',     // ที่อยู่ IP หรือชื่อโฮสต์ของ MySQL Server
-//   user: '347136',         // ชื่อผู้ใช้ MySQL
-//   password: 'Progresstracking1001',         // รหัสผ่าน MySQL
-//   database: 'seniorprojectprogresstracking_term1', // ชื่อฐานข้อมูลที่ใช้
-// });
-
+สร้าง pool connection ไปยังฐานข้อมูล MySQL
 const db = mysql.createPool({
   connectionLimit: 10, // จำนวน connection สูงสุดที่ pool สามารถจัดการได้
-  host: '0.0.0.0',     // ที่อยู่ IP หรือชื่อโฮสต์ของ MySQL Server
-  user: 'root',         // ชื่อผู้ใช้ MySQL
-  password: '',         // รหัสผ่าน MySQL
-  database: 'graduation_project', // ชื่อฐานข้อมูลที่ใช้
+  host: 'mysql-seniorprojectprogresstracking.alwaysdata.net',     // ที่อยู่ IP หรือชื่อโฮสต์ของ MySQL Server
+  user: '347136',         // ชื่อผู้ใช้ MySQL
+  password: 'Progresstracking1001',         // รหัสผ่าน MySQL
+  database: 'seniorprojectprogresstracking_term1', // ชื่อฐานข้อมูลที่ใช้
 });
+
+// const db = mysql.createPool({
+//   connectionLimit: 10, // จำนวน connection สูงสุดที่ pool สามารถจัดการได้
+//   host: '0.0.0.0',     // ที่อยู่ IP หรือชื่อโฮสต์ของ MySQL Server
+//   user: 'root',         // ชื่อผู้ใช้ MySQL
+//   password: '',         // รหัสผ่าน MySQL
+//   database: 'graduation_project', // ชื่อฐานข้อมูลที่ใช้
+// });
 
 // เชื่อมต่อกับ MySQL โดยใช้ Promise
 const connectToDatabase = () => {
